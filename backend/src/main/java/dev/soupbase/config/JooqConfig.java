67300f027ac144e23526b981abd76d93036ce6ec
p-danilin <peter.danilin@gmail.com>
2026-04-30 09:02:03 -0700

    fix tests

--- backend/src/main/java/dev/soupbase/config/JooqConfig.java
+++ backend/src/main/java/dev/soupbase/config/JooqConfig.java
@@ -2,6 +2,9 @@ package dev.soupbase.config;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.conf.RenderNameCase;
+import org.jooq.conf.RenderQuotedNames;
+import org.jooq.conf.Settings;
 import org.jooq.impl.DSL;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,9 @@ public class JooqConfig {
 
     @Bean
     public DSLContext dslContext(DataSource dataSource) {
-        return DSL.using(dataSource, SQLDialect.POSTGRES);
+        Settings settings = new Settings()
+                .withRenderQuotedNames(RenderQuotedNames.NEVER)
+                .withRenderNameCase(RenderNameCase.LOWER);
+        return DSL.using(dataSource, SQLDialect.POSTGRES, settings);
     }
 }
